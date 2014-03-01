c2975ba98d5d1de9c80f922733a80c1913c98363
David Tucker <dmtucker@ucsc.edu>
2014-02-28 21:12:44 -0800

    Implement coordinate retrieval. (0.2.0a)
    A GeoJSON file can now be specified through the query string of the URL
    and is retrieved using AJAX. Google Analytics code and the Google Maps
    API key have been removed until further notice for the following
    reasons: both will contain information specific to this application
    (not the open source code) and neither will be useful until deployment.
    Implementing a configuration file in JavaScript for this information
    would require more AJAX which does not seem to be worthwhile. Also,
    OpenStreetMap may replace Google Maps in the near future depending on
    the applicability of available plotting libraries such as Leaflet.js.
    Finally, only a single version of jQuery is needed, and it is going to
    be stored locally. Code to include the same version from a remote
    source has been removed.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
-function initialize() {
-    var mapOptions = {
-        center: new google.maps.LatLng(36.9720,-122.0263),
-        zoom: 14
-    };
-    var map = new google.maps.Map(document.getElementById("map-canvas"),mapOptions);
+var queryString = {}
+window.location.search.substr(1).split("&").forEach(
+    function (item) {
+        queryString[item.split("=")[0]] = item.split("=")[1]
+    }
+)
+if (!queryString.hasOwnProperty("data")) {
+    alert(
+        "A JSON file must be specified in the query string.\n" +
+        "\n" +
+        "Example\n" +
+        window.location.protocol+"//"+window.location.host+window.location.pathname+"?data=points.json"
+    );
+    window.location = "http://gsf.soe.ucsc.edu";
 }
-google.maps.event.addDomListener(window,"load",initialize);
+
+
+function initializeGoogleMaps () {
+    var map = new google.maps.Map(
+        document.getElementById("map-canvas"),
+        {
+            center: new google.maps.LatLng(36.9720,-122.0263),
+            zoom: 14
+        }
+    );
+}
+google.maps.event.addDomListener(window,"load",initializeGoogleMaps);
+
+
+var response = $.getJSON(
+    queryString.data,
+    function (data) {
+        console.log(data); // alert(JSON.stringify(data)); # TODO Remove this line when it is no longer useful.
+        // TODO Process and plot GeoJSON data.
+    }
+);
