59130fe20a1c4843a7333f594a8cec4f1fecb23f
David Tucker <dmtucker@ucsc.edu>
2014-03-07 16:08:08 -0800

    Switch from Google Maps to OpenStreetMap. (0.3.0a)
    OpenStreetMap has been substituted for Google Maps. Leaflet has been
    added to allow access to OSM (or any number of other mapping services
    if desired). Basic GeoJSON plotting has been implemented, but it needs
    refinement.

--- js/main.js
+++ js/main.js
@@ -6,31 +6,36 @@ window.location.search.substr(1).split("&").forEach(
 )
 if (!queryString.hasOwnProperty("data")) {
     alert(
-        "A JSON file must be specified in the query string.\n" +
+        "A GeoJSON file must be specified in the query string.\n" +
         "\n" +
         "Example\n" +
-        window.location.protocol+"//"+window.location.host+window.location.pathname+"?data=points.json"
+        window.location.protocol+"//"+window.location.host+window.location.pathname+"?data=points.geojson"
     );
     window.location = "http://gsf.soe.ucsc.edu";
 }
 
 
-function initializeGoogleMaps () {
-    var map = new google.maps.Map(
-        document.getElementById("map-canvas"),
-        {
-            center: new google.maps.LatLng(36.9720,-122.0263),
-            zoom: 14
-        }
-    );
-}
-google.maps.event.addDomListener(window,"load",initializeGoogleMaps);
 
+
+var map = L.map("map").locate({setView: true, maxZoom: 13});
+L.tileLayer(
+    "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
+    {
+        attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+    }
+).addTo(map);
 
 var response = $.getJSON(
     queryString.data,
     function (data) {
-        console.log(data); // alert(JSON.stringify(data)); # TODO Remove this line when it is no longer useful.
-        // TODO Process and plot GeoJSON data.
+        console.log(data); // TODO Remove this line when it is no longer useful.
+        L.geoJson(
+            data,
+            {
+                onEachFeature: function (feature, layer) {
+                    layer.bindPopup(feature.properties.name);
+                }
+            }
+        ).addTo(map);
     }
 );
