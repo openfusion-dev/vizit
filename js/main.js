e7ccff7a68c5da24b610fd6383131d3fcda4dd4a
David Tucker <dmtucker@ucsc.edu>
2014-03-08 23:56:11 -0800

    GeoJSON file visualizations. (0.4.0a)
    Vizit can now plot GeoJSON files. It uses the name property of GeoJSON
    Feature objects to create a popup for each waypoint and allows points
    to be customized by adding properties such as color and opacity (see
    http://leafletjs.com/reference.html#path-options for details). It also
    renders GeoJSON files with nested FeatureCollections, but they must
    have a very specific format that specifies a single Feature as the
    epicenter for an immediately proceeding FeatureCollection. One or many
    of these sets must be contained in a wrapper FeatureCollection. An
    example file has been included. Finally, a basic GeoJSON validator that
    allows for nested FeatureCollections has been included; however, it
    does not validate coordinates against corresponding type in Geometry
    objects. This code may be moved to a separate file in the future. Also,
    the map does not center itself appropriately yet.

--- js/main.js
+++ js/main.js
@@ -1,41 +1,158 @@
-var queryString = {}
-window.location.search.substr(1).split("&").forEach(
-    function (item) {
-        queryString[item.split("=")[0]] = item.split("=")[1]
+function isGeometry ( json ) {
+    if (json.hasOwnProperty("type")) {
+        if (json.type == "GeometryCollection") return isGeometryCollection(json);
+        else return $.inArray(
+            json.type,
+            [
+                "Point",
+                "MultiPoint",
+                "LineString",
+                "MultiLineString",
+                "Polygon",
+                "MultiPolygon"
+            ]
+        ) > -1 && json.hasOwnProperty("coordinates");
+        // TODO Validate coordinates based on type.
     }
-)
-if (!queryString.hasOwnProperty("data")) {
-    alert(
-        "A GeoJSON file must be specified in the query string.\n" +
-        "\n" +
-        "Example\n" +
-        window.location.protocol+"//"+window.location.host+window.location.pathname+"?data=points.geojson"
-    );
-    window.location = "http://gsf.soe.ucsc.edu";
+    else return false;
+}
+
+function isGeometryCollection ( json ) {
+    if (json.hasOwnProperty("type") && json.type == "GeometryCollection" &&
+        json.hasOwnProperty("geometries")) {
+        for (var i in json.geometries) {
+            if (!isGeometry(json.geometries[i])) return false;
+        }
+        return true;
+    }
+    else return false;
+}
+
+function isFeature ( json ) {
+    if (json.hasOwnProperty("type")) {
+        if (json.type == "FeatureCollection") return isFeatureCollection(json); // This does not conform to GeoJSON v1.0.
+        else return json.type == "Feature" &&
+                    json.hasOwnProperty("geometry") && isGeometry (json.geometry) &&
+                    json.hasOwnProperty("properties");
+    }
+    else return false;
+}
+
+function isFeatureCollection ( json ) {
+    if (json.hasOwnProperty("type") && json.type == "FeatureCollection" &&
+        json.hasOwnProperty("features")) {
+        for (var i in json.features) {
+            if (!isFeature(json.features[i])) return false;
+        }
+        return true;
+    }
+    else return false;
+}
+
+function isGeoJSON ( json ) {
+    return isFeature(json) || isGeometry(json);
+}
+
+
+function unvisualizable ( message ) {
+    console.log(message);
+    alert(message);
+    window.location.assign("http://gsf.soe.ucsc.edu");
+    throw new Error("unvisualizable"); // This prevents further execution after redirecting.
+}
+
+function plot ( map , GeoJSON ) {
+    L.geoJson(
+        GeoJSON,
+        {
+            pointToLayer: function ( feature , coordinate ) {
+                return L.circleMarker(
+                    coordinate,
+                    {
+                        className:   (typeof feature.properties.className   === "undefined") ? ""     : feature.properties.className,
+                        stroke:      (typeof feature.properties.stroke      === "undefined") ? true   : feature.properties.stroke,
+                        dashArray:   (typeof feature.properties.dashArray   === "undefined") ? null   : feature.properties.dashArray,
+                        //lineCap: null, //butt, round, square // TODO Figure out if these are applicable.
+                        //lineJoin: null, //miter, round, bevel // TODO Figure out if these are applicable.
+                        weight:      (typeof feature.properties.weight      === "undefined") ? 4      : feature.properties.weight,
+                        color:       (typeof feature.properties.color       === "undefined") ? "#000" : feature.properties.color,
+                        opacity:     (typeof feature.properties.opacity     === "undefined") ? 1.0    : feature.properties.opacity,
+                        radius:      (typeof feature.properties.radius      === "undefined") ? 8      : feature.properties.radius,
+                        fill:        (typeof feature.properties.fill        === "undefined") ? true   : feature.properties.fill,
+                        fillColor:   (typeof feature.properties.fillColor   === "undefined") ? "#5Af" : feature.properties.fillColor,
+                        fillOpacity: (typeof feature.properties.fillOpacity === "undefined") ? 0.8    : feature.properties.fillOpacity
+                    }
+                )
+            },
+            onEachFeature: function ( feature , layer ) {
+                layer.bindPopup(feature.properties.name);
+            }
+        }
+    ).addTo(map);
 }
 
 
 
 
-var map = L.map("map").locate({setView: true, maxZoom: 13});
-L.tileLayer(
-    "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
-    {
-        attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
+var queryString = {}
+window.location.search.substr(1).split("&").forEach(
+    function ( item ) {
+        queryString[item.split("=")[0]] = item.split("=")[1]
     }
-).addTo(map);
-
-var response = $.getJSON(
-    queryString.data,
-    function (data) {
-        console.log(data); // TODO Remove this line when it is no longer useful.
-        L.geoJson(
-            data,
+)
+if (!queryString.hasOwnProperty("data")) unvisualizable(
+    "Error: A GeoJSON file must be specified in the query string.\n" +
+    "\n" +
+    "Example\n" +
+    window.location.protocol+"//"+window.location.host+window.location.pathname+"?data=points.geojson"
+);
+else $.getJSON(queryString.data)
+    .done(function ( json ) {
+        if (!isFeatureCollection(json)) unvisualizable(
+            "Error: "+queryString.data+" must specify a FeatureCollection object!\n" +
+            "\n" +
+            "See http://geojson.org/ for more information."
+        );
+        var multiset = false;
+        for (var i in json.features) if (json.features[i]) multiset = true;
+        
+        var map = L.map("map").setView([0,0],3);
+        L.tileLayer(
+            "http://{s}.tile.osm.org/{z}/{x}/{y}.png",
             {
-                onEachFeature: function (feature, layer) {
-                    layer.bindPopup(feature.properties.name);
-                }
+                attribution: "&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
             }
         ).addTo(map);
-    }
-);
+        
+        if (multiset) {
+            for (var seti in json.features) {
+                if (!isFeatureCollection(json.features[seti])) unvisualizable(
+                    "Error: "+queryString.data+" is malformed."
+                );
+                var epicenter = json.features[seti].features[0];
+                L.circle(
+                    [epicenter.geometry.coordinates[1],epicenter.geometry.coordinates[0]],
+                    2500,
+                    { // TODO Add other options for appearance modification, and make them accessible from the GeoJSON file.
+                        weight: 1,
+                        color: "#000",
+                        opacity: 1.0,
+                        fillColor: "#fff",
+                        fillOpacity: 0.4
+                    }
+                ).addTo(map);
+                L.marker([epicenter.geometry.coordinates[1],epicenter.geometry.coordinates[0]])
+                    .bindPopup(epicenter.properties.name)
+                    .addTo(map);
+                plot(map,json.features[seti].features[1]);
+            }
+        }
+        else plot(map,json);
+    })
+    .fail(function ( response , error , statusText ) {
+        unvisualizable(
+            (response.status == 404) ?
+                "Error: " +queryString.data+" could not be found.":
+                "Error: " +statusText
+        );
+    })
