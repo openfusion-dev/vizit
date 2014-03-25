0f584a20d1cd5ab9bc0d9da06de62358542effdf
David Tucker <dmtucker@ucsc.edu>
2014-03-24 21:16:37 -0700

    Extend the GeoJSON validator to check coordinates. (2.2.1b)
    Another example file has also been added.

--- js/geojson.js
+++ js/geojson.js
@@ -1,54 +1,127 @@
-function isGeometry ( json ) {
-    if (json == null) return false;
-    if (json.type == null) return false;
-    if (json.type === "GeometryCollection") return isGeometryCollection(json);
-    else return $.inArray(
-        json.type,
-        [
-            "Point",
-            "MultiPoint",
-            "LineString",
-            "MultiLineString",
-            "Polygon",
-            "MultiPolygon"
-        ]
-    ) > -1 && Array.isArray(json.coordinates);
-    // TODO Validate coordinates based on type.
+function equalPositions ( a , b ) {
+    if (!isPosition(a) || !isPosition(b)) return false;
+    if (a.length != b.length) return false;
+    for (var numberi in a) {
+        if (a[numberi] != b[numberi]) return false;
+    }
+    return true;
 }
 
-function isGeometryCollection ( json ) {
-    if (json == null) return false;
-    if (json.type === "GeometryCollection" && Array.isArray(json.geometries)) {
-        for (var i in json.geometries) {
-            if (!isGeometry(json.geometries[i])) return false;
+
+function isPosition ( x ) {
+    if (!Array.isArray(x)) return false;
+    for (var numberi in x) {
+        if (typeof x[numberi] !== "number") return false;
+    }
+    return true;
+}
+
+
+function isMultiPoint ( x ) {
+    if (!Array.isArray(x)) return false;
+    for (var pointi in x) {
+        if (!isPosition(x[pointi])) return false;
+    }
+    return true;
+}
+
+
+function isLineString ( x ) {
+    if (!Array.isArray(x)) return false;
+    if (x.length < 2) return false;
+    return isMultiPoint(x)
+}
+
+
+function isLinearRing ( x ) {
+    if (!Array.isArray(x)) return false;
+    if (x.length < 4) return false;
+    if (!isLineString(x)) return false;
+    return equalPositions(x[0],x[x.length-1]);
+}
+
+
+function isMultiLineString ( x ) {
+    if (!Array.isArray(x)) return false;
+    for (var lineStringi in x) {
+        if (!isLineString(x[lineStringi])) return false;
+    }
+    return true;
+}
+
+
+function isPolygon ( x ) {
+    if (!Array.isArray(x)) return false;
+    for (var linearRingi in x) {
+        if (!isLinearRing(x[linearRingi])) return false;
+    }
+    return true;
+}
+
+
+function isMultiPolygon ( x ) {
+    if (!Array.isArray(x)) return false;
+    for (var polygoni in x) {
+        if (!isPolygon(x[polygoni])) return false;
+    }
+    return true;
+}
+
+
+function isGeometry ( x ) {
+    if (x == null) return false;
+    if (x.type == null) return false;
+    if (x.type === "GeometryCollection") return isGeometryCollection(x);
+    else {
+        switch (x.type) {
+            case "Point":           return isPosition(x.coordinates);
+            case "MultiPoint":      return isMultiPoint(x.coordinates);
+            case "LineString":      return isLineString(x.coordinates);
+            case "MultiLineString": return isMultiLineString(x.coordinates);
+            case "Polygon":         return isPolygon(x.coordinates);
+            case "MultiPolygon":    return isMultiPolygon(x.coordinates);
+            default:                return false;
+        }
+    }
+}
+
+
+function isGeometryCollection ( x ) {
+    if (x == null) return false;
+    if (x.type === "GeometryCollection" && Array.isArray(x.geometries)) {
+        for (var i in x.geometries) {
+            if (!isGeometry(x.geometries[i])) return false;
         }
         return true;
     }
     else return false;
 }
 
-function isFeature ( json ) {
-    if (json == null) return false;
-    if (json.type == null) return false;
-    return json.type === "Feature" &&
-           isGeometry (json.geometry) &&
-           typeof json.properties === "object";
+
+function isFeature ( x ) {
+    if (x == null) return false;
+    if (x.type == null) return false;
+    return x.type === "Feature" &&
+           isGeometry(x.geometry) &&
+           typeof x.properties === "object";
 }
 
-function isFeatureCollection ( json ) {
-    if (json == null) return false;
-    if (json.type === "FeatureCollection" && Array.isArray(json.features)) {
-        for (var i in json.features) {
-            if (!isFeature(json.features[i])) return false;
+
+function isFeatureCollection ( x ) {
+    if (x == null) return false;
+    if (x.type === "FeatureCollection" && Array.isArray(x.features)) {
+        for (var i in x.features) {
+            if (!isFeature(x.features[i])) return false;
         }
         return true;
     }
     else return false;
 }
 
-function isGeoJSON ( json ) {
-    if (json == null) return false;
-    return isGeometry(json) ||
-           isFeature(json)  ||
-           isFeatureCollection(json);
+
+function isGeoJSON ( x ) {
+    if (x == null) return false;
+    return isGeometry(x) ||
+           isFeature(x)  ||
+           isFeatureCollection(x);
 }
