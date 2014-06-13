bbbf3d2b50a4904027cf60d8132d8fd81498fbec
David Tucker <dmtucker@ucsc.edu>
2014-06-13 15:22:31 -0700

    Refactor the plain visualizer. (2.4.0b)
    This addresses the error the previous commit attempts (unsuccessfully)
    to fix.

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 OpenFusionVersion='0.1.0'
 VizitVersion='0.1.0'
 
-browserify "scripts/source/OpenFusion.js" > "scripts/geojson-$OpenFusionVersion.js"
-minify "scripts/geojson-$OpenFusionVersion.js" > "scripts/openfusion-$OpenFusionVersion.min.js"
-rm "scripts/geojson-$OpenFusionVersion.js"
+browserify "scripts/source/OpenFusion.js" > "scripts/openfusion-$OpenFusionVersion.js"
+minify "scripts/openfusion-$OpenFusionVersion.js" > "scripts/openfusion-$OpenFusionVersion.min.js"
+rm "scripts/openfusion-$OpenFusionVersion.js"
 
 browserify "scripts/source/Vizit.js" > "scripts/vizit-$VizitVersion.js"
 minify "scripts/vizit-$VizitVersion.js" > "scripts/vizit-$VizitVersion.min.js"
--- scripts/source/visualizers/plain.js
+++ scripts/source/visualizers/plain.js
@@ -1,59 +1,66 @@
 // This library depends on jQuery and Vizit.
 
 
-exports.version = '0.1.0';
+var GeoJSON = require('../node_modules/gjtk');
 
 
-exports.renderFeature = function ( Feature ) {
-    // Generate HTML for each GeoJSON Feature.
-    if (Feature.properties != null) {
-        //var canvas = this.canvas;
-        var article = document.createElement('article');
-        article.className = 'plain';
-        article.innerHTML = Vizit.HTMLFeature(Feature);
-        if (typeof Feature.properties.location === 'string') {
-            article.innerHTML +=
-                        '<address>'+Feature.properties.location+'</address>';
-            canvas.appendChild(article);
-        }
-        else if (Feature.geometry.type === 'Point') $.get(
-            'http://nominatim.openstreetmap.org/reverse' +
-                '?format=xml' +
-                '&lat='+Feature.geometry.coordinates[1].toString() +
-                '&lon='+Feature.geometry.coordinates[0].toString()
-        )
-            .done(
-                function ( XML ) {
-                    Feature.properties.location = $(XML).find('result').text();
-                    article.innerHTML +=
-                        '<address>'+Feature.properties.location+'</address>';
-                    canvas.appendChild(article);
-                }
-            )
-            .fail(
-                function ( response , error , statusText ) {
-                    console.error('Error: '+statusText);
-                }
-            );
-        else if (article.innerHTML != '') canvas.appendChild(article);
-        return article;
-    }
-}
-
-
-exports.visualizer = function ( geojson , canvasID ) {
-    // Instate a plain visualization.
-    var context = {canvas:document.getElementById(canvasID)};
-    exports.FeatureProcessor.bind(context);
-    Vizit.features(geojson).map(
-        function ( Feature ) {
-            var article = exports.renderFeature(Feature);
-            if (typeof article !== 'undefined') {
-                article.className +=
-                    (GeoJSON.isFeatureCollection(Feature.properties.related)) ?
-                        ' epicenter':
-                        ' aftershock';
+var plain = module.exports = {
+    
+    
+    version: '0.1.0',
+    
+    
+    render: function ( Feature , canvas ) {
+        // Generate HTML for each GeoJSON Feature.
+        if (Feature.properties != null) {
+            var article = document.createElement('article');
+            article.className = 'plain';
+            article.innerHTML = Vizit.HTMLFeature(Feature);
+            if (typeof Feature.properties.location === 'string') {
+                article.innerHTML +=
+                            '<address>'+Feature.properties.location+'</address>';
+                canvas.appendChild(article);
             }
+            else if (Feature.geometry.type === 'Point') $.get(
+                'http://nominatim.openstreetmap.org/reverse' +
+                    '?format=xml' +
+                    '&lat='+Feature.geometry.coordinates[1].toString() +
+                    '&lon='+Feature.geometry.coordinates[0].toString()
+            )
+                .done(
+                    function ( XML ) {
+                        Feature.properties.location = $(XML).find('result').text();
+                        article.innerHTML +=
+                            '<address>'+Feature.properties.location+'</address>';
+                        canvas.appendChild(article);
+                    }
+                )
+                .fail(
+                    function ( response , error , statusText ) {
+                        console.error('Error: '+statusText);
+                    }
+                );
+            else if (article.innerHTML != '') canvas.appendChild(article);
+            return article;
         }
-    );
+    },
+    
+    
+    visualizer: function ( geojson , canvasID ) {
+        // Instate a plain visualization.
+        var canvas = document.getElementById(canvasID);
+        Vizit.features(geojson).map(
+            function ( Feature ) {
+                var article = plain.render(Feature,canvas);
+                if (typeof article !== 'undefined') {
+                    article.className +=
+                        (GeoJSON.isFeatureCollection(Feature.properties.related)) ?
+                            ' epicenter':
+                            ' aftershock';
+                }
+            }
+        );
+    }
+    
+    
 }
